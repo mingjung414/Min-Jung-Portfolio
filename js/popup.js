1ac1b7a199f0479404b8be151322960a42858f72
mingjung414 <137235890+mingjung414@users.noreply.github.com>
2023-08-09 17:51:53 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('.popup1').click(function(){
+        $('.pop1').fadeIn();//.show()
+    });
+
+    $('.popup2').click(function(){
+        $('.pop2').fadeIn();//.show()
+    });
+
+    $('.popup3').click(function(){
+        $('.pop3').fadeIn();//.show()
+    });
+
+
+    
+    $('.popup a').click(function(){
+        $('.popup').fadeOut();//.hide()
+    });
+});
